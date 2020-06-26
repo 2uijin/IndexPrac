59abf4525c9c7faf0b06f6fa757298149eebf8f2
2uijin <49094082+2uijin@users.noreply.github.com>
2020-06-26 09:45:43 +0900

    초기 커밋.

---
+++ IndexPrac/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexPrac
+{
+    class Product { 
+        public int this[int i] {
+            get { return i; }
+            set { Console.WriteLine(i); }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p = new Product();
+            Console.WriteLine(p[3]);
+        }
+    }
+}
